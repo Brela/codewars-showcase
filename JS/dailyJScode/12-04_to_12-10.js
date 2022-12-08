1987439aa8d807e7ab7b5fcb82ec173ef41bb4b3
Breland <claybreland1@gmail.com>
2022-12-08 11:32:33 -0600

    second 6kyu codewars for 12-8 - tribonacci

--- JS/dailyJScode/12-04_to_12-10.js
+++ JS/dailyJScode/12-04_to_12-10.js
@@ -102,3 +102,33 @@ function alphabetPosition(text) {
 
 // console.log(alphabetPosition("The sunset sets at twelve o' clock."))
 // console.log(alphabetPosition("y5<{ic)f"))
+
+// ---------------------------- 12-08  6kyu #2 ------------------------------------------------
+
+/* you need to create a fibonacci function that given a signature array/list, returns the first n elements
+ - signature included of the so seeded sequence. */
+
+
+function tribonacci(sig, n) {
+
+    if (n <= 3) return sig.slice(0, n)
+    else
+        for (let i = 2; i < n - 1; i++) {
+            let seqToCheck = [sig[i - 2], sig[i - 1], sig[i]]
+            let currentPush = seqToCheck.reduce((acc, curr) => acc + curr)
+            sig.push(currentPush)
+        }
+    return sig
+}
+
+console.log(tribonacci([1, 1, 1], 10))   // [1,1,1,3,5,9,17,31,57,105]
+console.log(tribonacci([0, 0, 1], 10))  //  [0,0,1,1,2,4,7,13,24,44]  
+console.log(tribonacci([0, 0, 1], 2))  //  [0,0,1,1,2,4,7,13,24,44]  
+
+// best voted solution is like mine, but cleaner
+function tribonacci2(signature, n) {
+    for (var i = 0; i < n - 3; i++) { // iterate n times
+        signature.push(signature[i] + signature[i + 1] + signature[i + 2]); // add last 3 array items and push to trib
+    }
+    return signature.slice(0, n); //return trib - length of n
+}
