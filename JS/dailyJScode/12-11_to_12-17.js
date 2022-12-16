a8385af1ba09fc50b594a39fbdcae141556c1ea7
Breland <claybreland1@gmail.com>
2022-12-15 22:42:15 -0600

    12-15 #2

--- JS/dailyJScode/12-11_to_12-17.js
+++ JS/dailyJScode/12-11_to_12-17.js
@@ -199,3 +199,27 @@ function generateHashtag(str) {
     return r.length > 140 ? false : r;
 }
 //----------------------------------------------------------------------------------------
+
+//----------------------------------12-15  #2 -------------------------------------
+/* Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+
+ */
+console.log(disemvowel('letg go to succeed'))
+function disemvowel(str) {
+    let arr = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    str = str.split('').filter(el => !arr.includes(el)).join('')
+    return str;
+}
+console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
+function highAndLow(nums) {
+    nums = nums.split(' ').sort((a, b) => a - b)
+    return `${nums.pop()} ${nums.shift()}`
+}
