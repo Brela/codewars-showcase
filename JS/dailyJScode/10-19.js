8fb496e72be7d529b6ff89c5d9040bab1e0e032b
Breland <claybreland1@gmail.com>
2022-12-16 22:37:12 -0600

    moved js files to coding challenges repository

--- JS/dailyJScode/10-19.js
+++
@@ -1,3 +0,0 @@
-//SORT ARRAY AND DISPLAY
-
-const sortWords = arr => arr.sort((a, b) => a > b ? 1 : -1)
--- JS/dailyJScode/11-3.js
+++
@@ -1,9 +0,0 @@
-//calculate average
-
-// MINE is best
-
-function getAverage(marks) {
-    return marks.reduce((prev, current) => current += prev) / marks.length
-}
-
-console.log(getAverage([1, 2, 3, 4, 5,]));
